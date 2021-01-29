46b3b6481d041d369c8f4bd1749a4236d45034b8
WORK-PC\typeo <typeofb@gmail.com>
2021-01-29 15:07:36 +0900

    Sequelize ORM 적용

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,10 @@ let userModel = require('../models').User;
 
 // [GET] http://localhost:3000/user/typeofb
 let read = async function (req, res, next) {
-  await userModel.sequelize.query('SELECT * FROM USER WHERE NICKNAME = :nickname', {
-    type: userModel.sequelize.QueryTypes.SELECT,
-    replacements: {nickname: req.params.nickname}
+  await userModel.findAll({
+    where: {
+      nickname: req.params.nickname
+    }
   }).then(function (rows) {
     console.log(rows);
     return res.json(rows);
@@ -16,12 +17,9 @@ let read = async function (req, res, next) {
 
 // [POST] http://localhost:3000/user?nickname=typeofb&password=1234
 let insert = async function (req, res, next) {
-  await userModel.sequelize.query('INSERT INTO USER (NICKNAME, PASSWORD) VALUES (:nickname, :password)', {
-    type: userModel.sequelize.QueryTypes.INSERT,
-    replacements: {
-      nickname: req.query.nickname,
-      password: req.query.password
-    }
+  await userModel.create({
+    nickname: req.query.nickname,
+    password: req.query.password
   }).then(function (rows) {
     console.log(rows);
     return res.json(rows);
